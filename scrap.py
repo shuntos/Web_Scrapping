9ac70ad059f5025c3176092f561b75fef6e56aff
shuntos <santoshadhikari514@gmail.com>
2023-04-12 21:51:10 +0200

    Added multiple page handle

--- scrap.py
+++ scrap.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
 import time
 from bs4 import BeautifulSoup
 import os
@@ -24,6 +26,22 @@ language = "en_us"
 session = requests.Session()
 
 
+def merge_pdfs(pdf_list):
+    '''
+    Merger all the pdfs in list 
+    '''
+
+    pdf_merger = PdfFileMerger()
+
+    # Add PDFs to the merger
+    for pdf_path in pdf_paths:
+        with open(pdf_path, 'rb') as pdf_file:
+            pdf_merger.append(pdf_file)
+
+    # Write the merged PDF to the output file
+    with open(output_path, 'wb') as output_file:
+        pdf_merger.write(output_file)
+
 def refresh_page():
 
     driver.refresh()
@@ -36,6 +54,11 @@ def refresh_page():
 
     models[last_model].click()
     time.sleep(1)
+
+
+def print_beautify(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    print(soup.prettify())
 
 
 def extract_car_periods(html):
@@ -61,6 +84,12 @@ def extract_car_periods(html):
     return args_list, date_list
 
 
+def download_file(url, savepath):
+    if not os.path.exists(savepath):
+        print("Saving {} into {}".format(url, savepath))
+        response = session.get(url)
+        with open(savepath, 'wb') as f:
+            f.write(response.content)
 
 
 def download(beautiful_perioids, selenimum_periods,index ):
@@ -69,10 +98,14 @@ def download(beautiful_perioids, selenimum_periods,index ):
 
     outpath = outfolder+pdf_info[2]+"/"
 
+    
+
     if not os.path.exists(outpath):
         os.makedirs(outpath)
 
-    if pdf_info[0]=="pdfsimple":
+
+
+    if pdf_info[0]=="pdfsimple":#latest single pfds
         pdf_url = url+"modeles/"+pdf_info[1]+"/"+pdf_info[2]+"/"+pdf_info[3]+"/"+language+"/"+ pdf_info[1]+"_"+pdf_info[2]+"_"+pdf_info[3]+"_"+language+".pdf"
 
         pdf_file = pdf_url.split('/')[-1]
@@ -90,7 +123,7 @@ def download(beautiful_perioids, selenimum_periods,index ):
                 f.write(response.content)
 
 
-    elif pdf_info[0]=="pdf":
+    elif pdf_info[0]=="pdf": #Multiple pages pdfs
         selenimum_periods[i].click()
         time.sleep(1)
 
@@ -99,17 +132,84 @@ def download(beautiful_perioids, selenimum_periods,index ):
         image_url = soup.find("div", id="doczone").find('img').attrs['src']
 
 
-        print(image_url)
+        
+        img_path = outfolder+car_brand+img_folder+os.path.join(os.path.splitext(os.path.basename(image_url))[0], image_url.split('/')[-1])
+
+        if not os.path.exists(os.path.split(img_path)[0]):
+            os.makedirs(os.path.split(img_path)[0])
+
+
+        download_file(image_url, img_path)
+
+
+        pages = soup.find_all("a", class_="prglinkpdf")
+        pdf_links = []
+
+        for link in pages:
+            href = link.get('href')
+            if href and href.endswith('.pdf'):
+                pdf_links.append(href)
+
+
+        outpath = outfolder+car_brand+pdf_info[1]+"-"+pdf_info[2]+"/"+pdf_info[3]+"/"
+
+        if not os.path.exists(outpath):
+            print("Creating dir: ", outpath)
+            os.makedirs(outpath)
+
+        pdf_list = []
+        for pdf_link in pdf_links:
+            savepath = outfolder+car_brand+pdf_info[1]+"-"+pdf_info[2]+"/"+pdf_info[3]+"/"+pdf_link
+            source_path = url+"modeles/"+pdf_info[1]+"/"+pdf_info[2]+"/"+pdf_info[3]+"/"+language+"/"+ pdf_link
+
+            pdf_list.append(savepath)
+
+            download_file(source_path, savepath)
 
         time.sleep(2)
         refresh_page()
+
+
+    elif pdf_info[0]=="eGuide":
+        outpath = outfolder+car_brand+pdf_info[1]+"-"+pdf_info[2]+"/"+pdf_info[3]+"/"
+        if not os.path.exists(outpath):
+            os.makedirs(outpath)
+
+
+        selenimum_periods[i].click()
+        time.sleep(2)
+        soup_new = BeautifulSoup(driver.page_source, 'html.parser')
+
+
+        new_frame_url = url+soup_new.find("iframe")["src"]
+ 
+
+        new_driver = webdriver.Chrome(ChromeDriverManager().install())
+
+        new_driver.get(new_frame_url)
+
+        pdf_location = new_driver.find_element_by_id("eguide_pdf")
+        time.sleep(1)
+        pdf_location.click()
+        time.sleep(2)
+
+        new_driver.switch_to.window(new_driver.window_handles[-1])
+
+        savepath = outpath+new_driver.current_url.split('/')[-1]
+
+        download_file(new_driver.current_url, savepath)
+        new_driver.quit()
+
+        refresh_page()
+
 
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 url = "https://service.citroen.com/ACddb/"
 
-
+img_folder  = "Front_covers/"
+car_brand = "Citroen/"
 driver.get(url)
 
 
@@ -117,8 +217,10 @@ car_hovers = driver.find_elements_by_class_name("carbox") # returnls name of ite
 
 time.sleep(1)
 
-car_index = 4
+car_index = 6
 car_hover = car_hovers[car_index]
+
+print(car_hover.text)
 
 car_hover.click()
 
@@ -142,37 +244,39 @@ if len(models) == 0:
 else:
     for i in range(len(models)):
         last_model = i
-        print("Fetching pdfs from {}".format(models[i].get_attribute("title")))
 
-    
-        models[i].click()
+        if "dag" not  in models[i].get_attribute("title"):
+            print("Fetching pdfs from {}".format(models[i].get_attribute("title")))
 
-
-
-        # Fetching periods of each model
-
-        # Find all <li> elements under the <ul> element
-
-        beautiful_periods, date_periods = extract_car_periods(driver.page_source)
+        
+            models[i].click()
 
 
 
-        for i in range(len(beautiful_periods)):
-            element = driver.find_element_by_id("licarperiodelist")
-            selenium_period_list = element.find_elements_by_tag_name("li")
+            # Fetching periods of each model
 
-            print(beautiful_periods[i], "::::", date_periods[i])
-            download(beautiful_periods, selenium_period_list, i)
+            # Find all <li> elements under the <ul> element
+
+            beautiful_periods, date_periods = extract_car_periods(driver.page_source)
 
 
-        #Refreshing the page again
-        time.sleep(1)
-        driver.refresh()
-        time.sleep(1)
-        car_hovers = driver.find_elements_by_class_name("carbox") # returnls name of item_list
-        car_hover = car_hovers[car_index]
-        car_hover.click()
-        models = car_hover.find_elements_by_class_name("cartype") # returnls name of items
+
+            for i in range(len(beautiful_periods)):
+                element = driver.find_element_by_id("licarperiodelist")
+                selenium_period_list = element.find_elements_by_tag_name("li")
+
+                print(beautiful_periods[i], "::::", date_periods[i])
+                download(beautiful_periods, selenium_period_list, i)
+
+
+            #Refreshing the page again
+            time.sleep(1)
+            driver.refresh()
+            time.sleep(1)
+            car_hovers = driver.find_elements_by_class_name("carbox") # returnls name of item_list
+            car_hover = car_hovers[car_index]
+            car_hover.click()
+            models = car_hover.find_elements_by_class_name("cartype") # returnls name of items
 
 
 
